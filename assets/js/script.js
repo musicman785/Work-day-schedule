18d412dc6e8677086e9c947e2f7db50597a91801
Chris Salgado <musicman785@gmail.com>
2020-04-28 01:44:44 -0700

    Added save icons to buttons and styled the header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
-// Need a to work a work day scheduler with the  day time and date. Time is will be using an external library called moment. 
+// Add current time to paragraph tag
+$(document).ready(function () {
+    console.log("ready!");
+
+    var m = moment().format("dddd Do MMMM YYYY");
+    $("#currentDay").text(m);
+
+// I need a way to allow the user input to be printed on the text span. 
+
+// Then I need to add a click event to the button to store the input. 
+
+// I will then need a to set the input to the local storage so data is held if page is refreshed.
+
+// finally need a way to clear the user input once a task has been done.
 
 
+
+
+
+
+
+
+});
